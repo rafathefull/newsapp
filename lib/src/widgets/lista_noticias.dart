7c4f02be717f3d3310ca29fc7d0d8065613c5530
Rafa Carmona <rafa.thefull@gmail.com>
2021-01-09 19:28:11 +0100

    288. Diseño de las noticias parte 2

--- lib/src/widgets/lista_noticias.dart
+++ lib/src/widgets/lista_noticias.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:newsapp/src/models/news_models.dart';
+import 'package:newsapp/src/theme/tema.dart';
 
 class ListaNoticias extends StatelessWidget {
   final List<Article> noticias;
@@ -11,8 +14,155 @@ class ListaNoticias extends StatelessWidget {
     return ListView.builder(
       itemCount: this.noticias.length,
       itemBuilder: (context, index) {
-        return Text(this.noticias[index].title);
+        return _Noticia(noticia: noticias[index], index: index);
       },
+    );
+  }
+}
+
+class _Noticia extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _Noticia({@required this.noticia, @required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _TajetaTopBar(
+          noticia: noticia,
+          index: index,
+        ),
+        _TarjetaTitulo(
+          noticia: noticia,
+        ),
+        _TajetaImagen(noticia: noticia),
+        _TarjetaBody(noticia: noticia),
+        _TarjetaBotones(),
+        SizedBox(
+          height: 10,
+        ),
+        Divider()
+      ],
+    );
+  }
+}
+
+class _TajetaTopBar extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _TajetaTopBar({this.noticia, this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      margin: EdgeInsets.only(bottom: 10.0),
+      child: Row(
+        children: [
+          Text(
+            '${index + 1}. ',
+            style: TextStyle(color: miTema.accentColor),
+          ),
+          Text(
+            '${noticia.source.name}. ',
+            style: TextStyle(color: miTema.accentColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaTitulo({this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        noticia.title,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+      ),
+    );
+  }
+}
+
+class _TajetaImagen extends StatelessWidget {
+  final Article noticia;
+
+  const _TajetaImagen({this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+        child: Container(
+            child: (noticia.urlToImage != null)
+                ? FadeInImage(
+                    imageErrorBuilder: (context, error, stackTrace) {
+                      return Text(
+                        '   Image not load   ',
+                        style:
+                            TextStyle(fontSize: 25, color: miTema.accentColor),
+                      );
+                    },
+                    placeholder: AssetImage('assets/img/giphy.gif'),
+                    image: NetworkImage(noticia.urlToImage),
+                  )
+                : Image(
+                    image: AssetImage('assets/img/no-image.png'),
+                  )),
+      ),
+    );
+  }
+}
+
+class _TarjetaBody extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaBody({this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(noticia.description != null ? noticia.description : ''),
+    );
+  }
+}
+
+class _TarjetaBotones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RawMaterialButton(
+            onPressed: () {},
+            fillColor: miTema.accentColor,
+            child: Icon(Icons.star_border),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          ),
+          SizedBox(width: 10),
+          RawMaterialButton(
+            onPressed: () {},
+            fillColor: Colors.blue,
+            child: Icon(Icons.more),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          )
+        ],
+      ),
     );
   }
 }

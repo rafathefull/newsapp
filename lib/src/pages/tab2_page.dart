69c623776fd67b2329d9df0d05d9836dd2413ea6
Rafa Carmona <rafa.thefull@gmail.com>
2021-01-10 18:50:07 +0100

    Mostramos las categorias

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/src/models/category_model.dart';
+import 'package:newsapp/src/theme/tema.dart';
+import 'package:newsapp/src/widgets/lista_noticias.dart';
 import 'package:provider/provider.dart';
 import 'package:newsapp/src/services/news_service.dart';
 import 'package:newsapp/src/utils/stringExtension.dart';
@@ -7,10 +9,23 @@ import 'package:newsapp/src/utils/stringExtension.dart';
 class Tab2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final newsService = Provider.of<NewsService>(context);
+
     return SafeArea(
       child: Scaffold(
         body: Column(
-          children: [Expanded(child: _ListaCategorias())],
+          children: [
+            _ListaCategorias(),
+            if (!newsService.isLoading)
+              Expanded(
+                  child: ListaNoticias(
+                      newsService.getArticulosCategoriasSeleccionada)),
+            if (newsService.isLoading)
+              Expanded(
+                  child: Center(
+                child: CircularProgressIndicator(),
+              ))
+          ],
         ),
       ),
     );
@@ -22,27 +37,31 @@ class _ListaCategorias extends StatelessWidget {
   Widget build(BuildContext context) {
     final categories = Provider.of<NewsService>(context).categories;
 
-    return ListView.builder(
-      physics: BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      itemCount: categories.length,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              _categoryButton(categoria: categories[index]),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                categories[index].name.capitalize(),
-                softWrap: false,
-              )
-            ],
-          ),
-        );
-      },
+    return Container(
+      height: 80.0,
+      width: double.infinity,
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              children: [
+                _categoryButton(categoria: categories[index]),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  categories[index].name.capitalize(),
+                  softWrap: false,
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -54,21 +73,26 @@ class _categoryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final newsService = Provider.of<NewsService>(context);
+
     return GestureDetector(
       // Controlamos el click de los iconos
       onTap: () {
-        print('${categoria.name}');
+        // Cuando estamos en un onTap, click, etc.. el evento no se tiene que redibujar
+        // final newsService = Provider.of<NewsService>(context);
+        final newsService = Provider.of<NewsService>(context, listen: false);
+        newsService.selectedCategory = categoria.name;
       },
       child: Container(
-          width: 40,
-          height: 40,
-          margin: EdgeInsets.symmetric(horizontal: 10),
-          decoration:
-              BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-          child: Icon(
-            categoria.icon,
-            color: Colors.black54,
-          )),
+        width: 40,
+        height: 40,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+        child: Icon(categoria.icon,
+            color: (newsService.selectedCategory == this.categoria.name
+                ? miTema.accentColor
+                : Colors.black54)),
+      ),
     );
   }
 }
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -4,11 +4,14 @@ import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/models/news_models.dart';
 import 'package:http/http.dart' as http;
 
-final _URL_NEWS = 'https://newsapi.org/v2';
-final _APIKEY = '400716e1b12149a786c3b6a28f628e81';
+final _url_news = 'https://newsapi.org/v2';
+final _apikey = '400716e1b12149a786c3b6a28f628e81';
 
 class NewsService with ChangeNotifier {
-  String country = 'ar';
+  String country = 'us';
+  String _selectedCategory = 'business';
+
+  bool _isLoading = true; // Si esta cargando el segundo tab2
 
   List<Article> headlines = [];
 
@@ -22,16 +25,52 @@ class NewsService with ChangeNotifier {
     Category(FontAwesomeIcons.tv, 'entertainment'),
   ];
 
+  Map<String, List<Article>> categoryArticles = {};
+
   NewsService() {
     this.getTopHeadLines();
+    categories.forEach((element) {
+      this.categoryArticles[element.name] = [];
+    });
   }
 
+  bool get isLoading => this._isLoading;
+
+  get selectedCategory => this._selectedCategory;
+
+  set selectedCategory(String valor) {
+    this._selectedCategory = valor;
+    this._isLoading = true;
+    this.getArticlesByCategory(valor);
+    notifyListeners();
+  }
+
+  List<Article> get getArticulosCategoriasSeleccionada =>
+      this.categoryArticles[this.selectedCategory];
+
   getTopHeadLines() async {
-    final url = '$_URL_NEWS/top-headlines?country=$country&apiKey=$_APIKEY';
+    final url = '$_url_news/top-headlines?country=$country&apiKey=$_apikey';
     final resp = await http.get(url);
     final newsResponse = newsResponseFromJson(resp.body);
 
     this.headlines.addAll(newsResponse.articles);
+    notifyListeners(); // Notificamos a quien este en la escucha.
+  }
+
+  // Solo cogeremos una vez los articulos.
+  getArticlesByCategory(String category) async {
+    if (this.categoryArticles[category].length > 0) {
+      this._isLoading = false;
+      notifyListeners();
+      return this.categoryArticles[category];
+    }
+    final url =
+        '$_url_news/top-headlines?country=$country&apiKey=$_apikey&category=$category';
+    final resp = await http.get(url);
+    final newsResponse = newsResponseFromJson(resp.body);
+
+    this.categoryArticles[category].addAll(newsResponse.articles);
+    this._isLoading = false;
     notifyListeners(); // Notificamos a quien este en la escucha.
   }
 }
